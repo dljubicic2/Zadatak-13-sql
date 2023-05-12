826b335c58fa2e76ae64961c5d69165832c15da0
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2023-05-12 18:03:56 +0200

    Add files via upload

---
+++ zadatak 13 sql.sql
@@ -0,0 +1,36 @@
+﻿-- zadatak 13
+use master
+drop database if exists zadacasql;
+go
+create database zadacasql;
+use zadacasql;
+
+create table predavanje(
+	id int not null primary key identity(1,1),
+	razred int not null,
+	predmet varchar(50) not null,
+	naziv varchar(50) null
+);
+create table razred(
+	id int not null primary key identity(1,1),
+	ucenik int not null,
+	profesor int not null,
+	brojdvorane int null
+);
+create table ucenik(
+	id int not null primary key identity(1,1),
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
+	brojuimeniku int not null
+);
+create table profesor(
+	id int not null primary key identity(1,1),
+	ime varchar(50) not null,
+	prezime varchar(50) not null
+);
+
+-- veze između tablica
+
+alter table predavanje add foreign key (razred) references razred(id);
+alter table razred add foreign key (ucenik) references ucenik(id);
+alter table razred add foreign key (profesor) references profesor(id);
